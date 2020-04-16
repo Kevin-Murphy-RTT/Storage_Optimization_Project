c96066d042daf74a370784a5a700753907c491f3
Kevin-Murphy-RTT <60367636+Kevin-Murphy-RTT@users.noreply.github.com>
2020-04-15 20:24:37 -0400

    Update Salt_scp_module.sh

--- Modules/Salt_scp_module.sh
+++ Modules/Salt_scp_module.sh
@@ -3,7 +3,9 @@
 #Format: salt.modules.scp_mod.get(remote_path, local_path='', recursive=False, preserve_times=False, **kwargs)
 
 #Define list of servers in wave
-servers=(‘ef-wc-ecco-l-stage1-n1’ ‘ef-wc-ecco-l-stage1-n2’)
+SERVERS = [
+    'serv1'
+]
 
 #Salt SCP Format: salt.modules.scp_mod.get(remote_path, local_path='', recursive=False, preserve_times=False, **kwargs)
 
